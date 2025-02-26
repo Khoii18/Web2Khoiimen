22fb9fd7196d4b297735cf8b73943937c8b83c49
Khoii18 <KhoiNAGCH21106@fpt.edu.vn>
2025-02-26 15:58:00 +0700

    khoimen

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log('database connected'))
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/Khoisan-Fashion`)
+    await mongoose.connect(`${process.env.MONGODB_URL}/Khoiimen-Fashion`)
 }
 
 export default connectDB
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -7,52 +7,51 @@ export const registerUser = async (req, res) => {
     try {
         const { username, email, phone, password_1, password_2, dob } = req.body
 
-        console.log(username,email, phone, password_1, password_2, dob )
 
-        // if (!username || !email || !phone || !password_1 || !password_2 || !dob) {
-        //     return res.json({ success: false, message: 'Hãy Điền Đầy Đủ Thông Tin' })
-        // }
+        if (!username || !email || !phone || !password_1 || !password_2 || !dob) {
+            return res.json({ success: false, message: 'Hãy Điền Đầy Đủ Thông Tin' })
+        }
 
-        // if (!validator.isEmail(email)) {
-        //     return res.status(400).json({ success: false, message: "Hãy Điền Email Hợp Lệ" })
-        // }
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ success: false, message: "Hãy Điền Email Hợp Lệ" })
+        }
 
-        // const isUser = await userModel.findOne({ email })
+        const isUser = await userModel.findOne({ email })
 
-        // if (isUser) {
-        //     return res.json({ success: false, message: 'Email này đã tồn tại' })
-        // }
+        if (isUser) {
+            return res.json({ success: false, message: 'Email này đã tồn tại' })
+        }
 
-        // const isPhone = await userModel.findOne({ phone })
-        // if (isPhone) {
-        //     return res.json({ success: false, message: 'Số điện thoại này đã tồn tại' })
-        // }
+        const isPhone = await userModel.findOne({ phone })
+        if (isPhone) {
+            return res.json({ success: false, message: 'Số điện thoại này đã tồn tại' })
+        }
 
-        // if (phone.length !== 10) {
-        //     return res.json({ success: false, message: 'Hãy Điền Số Điện Thoại Hợp Lệ ' })
-        // }
+        if (phone.length !== 10) {
+            return res.json({ success: false, message: 'Hãy Điền Số Điện Thoại Hợp Lệ ' })
+        }
 
-        // if (password_1.length < 8) {
-        //     return res.json({ success: false, message: 'Mật Khẩu Không Đủ Mạnh' })
-        // }
+        if (password_1.length < 8) {
+            return res.json({ success: false, message: 'Mật Khẩu Không Đủ Mạnh' })
+        }
 
-        // if (password_1 !== password_2) {
-        //     return res.json({ success: false, message: 'Mật Khẩu Không Giống Nhau' })
-        // }
+        if (password_1 !== password_2) {
+            return res.json({ success: false, message: 'Mật Khẩu Không Giống Nhau' })
+        }
 
-        // const salt = await bcrypt.genSalt(10)
-        // const hashedPassword = await bcrypt.hash(password_1, salt)
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password_1, salt)
 
-        // const userData = {
-        //     username,
-        //     email,
-        //     phone,
-        //     password: hashedPassword,
-        //     dob
-        // }
+        const userData = {
+            username,
+            email,
+            phone,
+            password: hashedPassword,
+            dob
+        }
 
-        // const newUser = new userModel(userData)
-        // await newUser.save()
+        const newUser = new userModel(userData)
+        await newUser.save()
 
         res.json({ success: true })
 
